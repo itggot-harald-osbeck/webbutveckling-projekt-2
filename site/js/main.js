ac6c935bb76af2983c336d4faa73680bca17fadb
itggot-harald-osbeck <harald.osbeck@elev.ga.ntig.se>
2019-01-17 15:36:56 +0100

    Ändringar

---
+++ site/js/main.js
@@ -0,0 +1,28 @@
+var countDownDate = new Date("Dec 24, 2019 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "D " + hours + "H "
+  + minutes + "M " + seconds + "S ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+function amenu(event){
+  let thing = document.querySelector(".amenu")
+  thing.classList.toggle("amenuactivate")
+
+}
