9deb8ded0c2e63e81194058aa2f32aaab2cf3cb1
itggot-harald-osbeck <harald.osbeck@elev.ga.ntig.se>
2019-01-24 15:42:14 +0100

    Ändringar

--- site/js/main.js
+++ site/js/main.js
@@ -12,8 +12,12 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("demo").innerHTML = days + "D " + hours + "H "
-  + minutes + "M " + seconds + "S ";
+  // document.getElementById("demo").innerHTML = days + "<br>Dagar<br> " + hours + "<br>Timmar<br> "
+  // + minutes + "<br>Minuter<br> " + seconds + "<br>Sekunder<br> ";
+  document.getElementById("dagar").innerHTML = days + "<br>Dagar<br> ";
+  document.getElementById("timmar").innerHTML = hours + "<br>Timmar<br> ";
+  document.getElementById("minuter").innerHTML = minutes + "<br>Minuter<br> ";
+  document.getElementById("sekunder").innerHTML = seconds + "<br>Sekunder<br> ";
 
   if (distance < 0) {
     clearInterval(x);
