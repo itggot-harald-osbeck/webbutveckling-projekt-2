79ed141cb61c2ae3c7d9fd745a3b7dabfeecb741
itggot-harald-osbeck <harald.osbeck@elev.ga.ntig.se>
2019-01-29 23:00:28 +0100

    Slutgiltiga ändringar(tror jag)

--- site/js/main.js
+++ site/js/main.js
@@ -11,14 +11,14 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // document.getElementById("demo").innerHTML = days + "<br>Dagar<br> " + hours + "<br>Timmar<br> "
-  // + minutes + "<br>Minuter<br> " + seconds + "<br>Sekunder<br> ";
+  
   document.getElementById("dagar").innerHTML = days; //"<br>Dagar<br> ";
   document.getElementById("timmar").innerHTML = hours; //"<br>Timmar<br> ";
   document.getElementById("minuter").innerHTML = minutes;// + "<br>Minuter<br> ";
   document.getElementById("sekunder").innerHTML = seconds;// + "<br>Sekunder<br> ";
-
+  document.getElementById("demo").innerHTML = days + " Dagar " + hours + " Timmar "
+  + minutes + " Minuter " + seconds + " Sekunder ";
+  
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
